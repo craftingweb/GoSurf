9df0a24d3ba87ac8d07059073df33ea9e8fbe00a
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-12-09 14:53:50 -0500

    header slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
-$(function(){
-
-
-
-  
+$(function () {
+  $(".header__slider").slick({
+    infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows-left" src="/img/arrow-left.svg"></img>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows-right" src="img/arrow-right.svg"></img>',
+  });
 });
