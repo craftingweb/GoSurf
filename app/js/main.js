07de1d8ee56a9fdd77ca3f5522842621a717793c
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-12-19 19:11:00 -0500

    travel slider business logic

--- app/js/main.js
+++ app/js/main.js
@@ -33,4 +33,12 @@ $(function () {
     asNavFor: ".surf-slider",
     focusOnSelect: true,
   });
+  $(".travel__slider").slick({
+    infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows-left" src="/img/arrow-left.svg"></img>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows-right" src="img/arrow-right.svg"></img>',
+  });
 });
